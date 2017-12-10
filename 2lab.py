11d92cd307b68ee2fbc8dc29c24a186ddf59a8b8
kshcherbina <shcherbina.art@gmail.com>
2017-12-10 23:32:58 +0600

    final

--- 2lab.py
+++ 2lab.py
@@ -6,17 +6,17 @@ def xls_to_txt(project_name,author_name,f_name):
     #project_name = input('Введите название:')
     #author_name = input('Введите имя автора:')
     f = open('result.mkb', 'w')
-    f.write(project_name+'\n'+author_name+'\n\n')
+    f.write('База данных о '+project_name+'.'+'\n'+'Автор: '+author_name+'.'+'\n\n')
     
     
     try:
             
-        table = xlrd.open_workbook(f_name)
+        table = xlrd.open_workbook(f_name) #открытие файла
         
     except IOError:
         print('No such file or directory')
         return 0
-    worksheet = table.sheet_by_index(0)
+    worksheet = table.sheet_by_index(0) 
 
     count = 1
 
@@ -27,9 +27,11 @@ def xls_to_txt(project_name,author_name,f_name):
 
         if i>2 and i<worksheet.nrows-1:
             for j in range(0,len(row)-1,2):
+                #записываем название и коэффициенты
                 if j<2:
                     f.write(row[0])
                     f.write(row[1])
+                #записываем ответы
                 else:
                     
                     f.write(','+str(count)+','+str(row[j])+','+str(row[j+1]))
@@ -37,7 +39,7 @@ def xls_to_txt(project_name,author_name,f_name):
                 
             count = 1
             f.write('\n')
-
+        #Запись вопросов 
         elif i == 1:
             for j in range(len(row)):
                 if row[j] !='' and j!=1:
@@ -45,14 +47,16 @@ def xls_to_txt(project_name,author_name,f_name):
             f.write('\n')
 
     f.close()
+    print('Success')
 
 def GUI():
     root = Tk()
-    label = Label(root, text = 'Название',width=20,bd=3)
+    root.title(u'Convector in mkb')
+    label = Label(root, text = 'База данных о',width=20,bd=3)
     ent = Entry(root,width=20,bd=3)
-    label1 = Label(root, text = 'Имя',width=20,bd=3)
+    label1 = Label(root, text = 'Автор',width=20,bd=3)
     ent1 = Entry(root,width=20,bd=3)
-    label2 = Label(root, text = 'Путь к фалу',width=20,bd=3)
+    label2 = Label(root, text = 'Путь к файлу',width=20,bd=3)
     ent2 = Entry(root,width=20,bd=3)
 
     label.pack()
@@ -67,7 +71,8 @@ def GUI():
           width=30,height=5, #ширина и высота
           bg="white",fg="blue") #цвет фона и надписи
 
-    but.bind('<Button-1>',lambda event: xls_to_txt(ent.get(),ent1.get(),ent2.get()))
+    but.bind('<Button-1>', lambda event: xls_to_txt(ent.get(),ent1.get(),ent2.get()))
+
 
 
     but.pack()
